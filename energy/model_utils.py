4708136aa7495eda666ba92c163f0123d97af97d
Kaido997 <78274772+Kaido997@users.noreply.github.com>
2022-05-10 12:06:23 +0200

    Update model_utils.py

--- energy/model_utils.py
+++ energy/model_utils.py
@@ -1,7 +1,7 @@
 import requests
 from web3 import Web3
 
-URL = "https://955cf8ae-5a74-4e4d-85d4-e8095a57862a.mock.pstmn.io"
+URL = ""
 
 
 def get_data_from_endpoint(URL):
@@ -10,10 +10,10 @@ def get_data_from_endpoint(URL):
 
 
 def makeTransaction(values):
-    ADDRESS = "0x227C6160b14E531c43Be4CB3f7d7dC75a1F6a471"
-    KEY = "0x743d867cee41c4c33d1f98514396d52822034bf7dfc1fe1e30fc671e0850535a"
+    ADDRESS = ""
+    KEY = ""
     WEB3 = Web3(Web3.HTTPProvider(
-        'https://ropsten.infura.io/v3/cecd52cfb8614b9ba5b06c12167ce00f'))
+        ''))
     nonce = WEB3.eth.getTransactionCount(ADDRESS)
     gasPrice = WEB3.eth.gasPrice
     value = WEB3.toWei(0, 'ether')
