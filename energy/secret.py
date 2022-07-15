b30ed3ccf677fb1a404f37fb0368d6c5c74bbc91
Kaido997 <78274772+Kaido997@users.noreply.github.com>
2022-07-16 01:49:34 +0200

    Update secret.py

--- energy/secret.py
+++ energy/secret.py
@@ -1,7 +1,7 @@
-ADDRESS = "0x227C6160b14E531c43Be4CB3f7d7dC75a1F6a471"
-KEY ="0x743d867cee41c4c33d1f98514396d52822034bf7dfc1fe1e30fc671e0850535a"
-URL = "https://955cf8ae-5a74-4e4d-85d4-e8095a57862a.mock.pstmn.io"#<--- Endpoint url
-ROPSTEN = "https://ropsten.infura.io/v3/cecd52cfb8614b9ba5b06c12167ce00f"
+ADDRESS = ""
+KEY = ""
+URL = ""#<--- Endpoint url
+ROPSTEN = ""
 
 '''Add your ethereum ropsten address, key and url 
 and in url your endpointurl'''
